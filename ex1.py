0696ba6d6db2ba641901f23fcb95074cdb39f063
Joongsoo Lee <joongsoo@etri.re.kr>
2016-10-05 20:01:38 +0900

    exercise code added

---
+++ ex1.py
@@ -0,0 +1,15 @@
+# Copyright 2016 Joongsoo Lee @ ETRI. All Rights Reserved.
+
+"""An exercise program.
+
+Make a program that returns average of fist elements of inner list.
+
+For example,
+when given grad_and_vars = [[1, 5], [3, 7]]
+
+The result should be 2.
+"""
+
+import tensorflow as tf
+
+grad_and_vars = [[1.1, 2.0], [0.7, 0.9], [0.6, 0.4], [1.2, 5.4]]
