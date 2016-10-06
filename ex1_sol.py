874c933f6bb64d02824642cbdc77481f85d8fb84
Joongsoo Lee <joongsoo@etri.re.kr>
2016-10-06 12:25:58 +0900

    train modified

--- ex1_sol.py
+++ ex1_sol.py
@@ -14,15 +14,22 @@ import tensorflow as tf
 
 grad_and_vars = [[1.1, 2.0], [0.7, 0.9], [0.6, 0.4], [1.2, 5.4]]
 
+sess = tf.Session()
+
 grads = []
 
 for g, _ in grad_and_vars:
     expanded_g = tf.expand_dims(g, 0)
-
+#    print(sess.run(expanded_g))
     grads.append(expanded_g)
+#    grads.append(g)
 
+
+#print(sess.run(grads))
 grad = tf.concat(0, grads)
+#print(sess.run(grads))
+#grad = tf.reduce_mean(grads, 0)
 grad = tf.reduce_mean(grad, 0)
 
-sess = tf.Session()
+#sess = tf.Session()
 print(sess.run(grad))
